685c31b36fc85b0fa7da48a924868ed783e2c8d2
Axel Bylund <axel@bazooka.se>
2016-11-17 11:17:44 +0100

    first commit, parallax in place

---
+++ wp-content/themes/mytheme/footer.php
@@ -0,0 +1,12 @@
+<hr>
+
+      <footer>
+        <p>© Company 2012</p>
+      </footer>
+<?php if(!is_front_page()):?>
+    </div> <!-- /container -->
+<?php endif;?>
+    <?php wp_footer(); ?>
+
+  </body>
+</html>
---
+++ wp-content/themes/mytheme/front-page.php
@@ -0,0 +1,21 @@
+<?php get_header(); ?>
+    <div class="parallax">
+        <div id="group1" class="parallax__group">
+            <div class="parallax__layer parallax__layer--base">
+                <div class="title">Base Layer</div>
+            </div>
+            <div class="parallax__layer parallax__layer--back"> </div>
+        </div>
+        <div id="group2" class="parallax__group">
+            <div class="parallax__layer parallax__layer--fore">
+                <div class="title">Foreground Layer</div>
+            </div>
+            <div class="parallax__layer parallax__layer--base"> </div>
+        </div>
+        <div class="front-page-bottom-container">
+            <div class="left">
+                <h1>Left</h1> </div>
+            <div class="right">
+                <h1>Right</h1> </div>
+        </div>
+    </div>
