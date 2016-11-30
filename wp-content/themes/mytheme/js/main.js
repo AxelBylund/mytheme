830f07239379cce489209d88b48958664de1e830
Axel Bylund <axel@bazooka.se>
2016-11-30 14:43:18 +0100

    more shit

--- wp-content/themes/mytheme/js/main.js
+++ wp-content/themes/mytheme/js/main.js
@@ -1,7 +1,5 @@
 $(document).ready(function () {
     console.log('main.js loaded');
-});
-$(document).ready(function () {
     $('#select_image_button').click(function () {
         wp.media.editor.send.attachment = function (attachment, data) {
             $('#background-image').val(data.id);
@@ -9,8 +7,17 @@ $(document).ready(function () {
         }
         wp.media.editor.open('#select_image_button');
     });
-    $title = $('#first-title');
-    $(document).scroll(function() {
-        $title.toggleClass('.title-box-scroll')
-    })
+    $(function () {
+        //caches a jQuery object containing the header element
+        var brand = $(".navbar-inverse .brand");
+        $('.parallax').scroll(function () {
+            var scroll = $('.parallax').scrollTop();
+            if (scroll >= 270 && scroll <= 420) {
+                brand.addClass("scroll-color");
+            }
+            else if (scroll <= 270 || scroll >= 420) {
+                brand.removeClass('scroll-color');
+            }
+        });
+    });
 });
