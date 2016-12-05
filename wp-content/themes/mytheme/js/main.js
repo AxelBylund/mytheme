685b1401dc84ef6a625a99c590a193c76e23c10e
Axel Bylund <axel@bazooka.se>
2016-12-05 18:11:32 +0100

    fixed scroll color

--- wp-content/themes/mytheme/js/main.js
+++ wp-content/themes/mytheme/js/main.js
@@ -10,14 +10,18 @@ $(document).ready(function () {
     $(function () {
         //caches a jQuery object containing the header element
         var brand = $(".navbar-inverse .brand");
+        var menuItems = $('.navbar .nav > li > a');
         $('.parallax').scroll(function () {
             var scroll = $('.parallax').scrollTop();
-            if (scroll >= 270 && scroll <= 420) {
-                brand.addClass("scroll-color");
+            if (scroll >= 265 && scroll <= 420) {
+                brand.addClass('scroll-color');
+                menuItems.addClass('scroll-color');
             }
-            else if (scroll <= 270 || scroll >= 420) {
+            else if (scroll <= 265 || scroll >= 420) {
                 brand.removeClass('scroll-color');
+                menuItems.removeClass('scroll-color');
             }
+            console.log(scroll);
         });
     });
 });
